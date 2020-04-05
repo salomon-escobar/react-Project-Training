129464f8ca8bc038d6df8a8cebdcf37575b81474
salomon.escobar <salomon.escobar@company-corp.com>
2020-04-05 11:53:51 -0500

    Updates

---
+++ precache-manifest.c29a5b29d8a177eb987af7efbf60bbb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a287e562c4d98c3dc7124b2f27f94f",
+    "url": "/react-Project-Training/index.html"
+  },
+  {
+    "revision": "2978aaa666a6d065d0a5",
+    "url": "/react-Project-Training/static/css/main.96d0dbe6.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/react-Project-Training/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-Project-Training/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2978aaa666a6d065d0a5",
+    "url": "/react-Project-Training/static/js/main.c87b264b.chunk.js"
+  },
+  {
+    "revision": "112010225a5c95b21176",
+    "url": "/react-Project-Training/static/js/runtime-main.46db3624.js"
+  }
+]);
